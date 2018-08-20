10fba7d15e00928674fe20e560b5d575367a232b
Tristan Brown <tgbrown@stanford.edu>
2018-08-20 23:33:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,9 +17,11 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-end 
+    puts "Currently serving #{katz_deli[0]}"
+  end 
+  katz.deli.pop
 end
