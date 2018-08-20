87cb487754b8e0b87278e4b86de6936fba945bca
Tristan Brown <tgbrown@stanford.edu>
2018-08-20 23:32:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,8 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving
-  if katz_deli.length == 0 
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
-  else 
-    
+  else
+end 
 end
